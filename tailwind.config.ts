3bed0bdfb18b56b8caa1e839dc546e6a4abcab10
Valik <valik3201@gmail.com>
2024-06-27 20:27:00 +0200

    style(tailwind): Updated tailwind.config by adding font-serif (Lora)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,8 @@ const config = {
     },
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+        serif: ["var(--font-lora)", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
