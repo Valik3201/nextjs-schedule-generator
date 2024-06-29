305de15247ec38c078e75371b05c5a7c485a14c8
Valik <valik3201@gmail.com>
2024-06-29 14:20:04 +0200

    feat(i18n): Added Locale Provider

---
+++ components/providers/LocaleProvider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { createContext, useContext, ReactNode } from "react";
+
+interface LocaleContextProps {
+  locale: string;
+  dictionary: Record<string, any>;
+}
+
+const LocaleContext = createContext<LocaleContextProps | undefined>(undefined);
+
+interface LocaleProviderProps {
+  children: ReactNode;
+  locale: string;
+  dictionary: Record<string, any>;
+}
+
+export const LocaleProvider = ({
+  children,
+  locale,
+  dictionary,
+}: LocaleProviderProps) => {
+  return (
+    <LocaleContext.Provider value={{ locale, dictionary }}>
+      {children}
+    </LocaleContext.Provider>
+  );
+};
+
+export const useLocale = (): LocaleContextProps => {
+  const context = useContext(LocaleContext);
+  if (!context) {
+    throw new Error("useLocale must be used within a LocaleProvider");
+  }
+  return context;
+};
