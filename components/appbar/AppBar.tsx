0b3ccc29c5827b518ea1f9fc3a9d4d3f7ad2f1a5
Valik <valik3201@gmail.com>
2024-07-01 23:15:56 +0200

    style(appbar): Updated icon color in AppBar

--- components/appbar/AppBar.tsx
+++ components/appbar/AppBar.tsx
@@ -8,7 +8,7 @@ export default function AppBar() {
       <div className="container md:px-20 lg:px-52 w-full">
         <div className="flex justify-between items-center px-4 py-3 w-full bg-white/40 dark:bg-black/40 backdrop-filter backdrop-blur-lg rounded-full border shadow-sm">
           <div className="flex items-center gap-1 pl-4">
-            <FileSpreadsheet />
+            <FileSpreadsheet className="text-emerald h-6 w-6" />
             <h2 className="scroll-m-20 text-md font-bold tracking-tight text-center">
               Schedule Generator
             </h2>
